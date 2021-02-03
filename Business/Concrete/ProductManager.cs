4f1fe7d5d9b29a53515b347c8b8d022985b8a480
sametsolaklar <61630850+sametsolaklar@users.noreply.github.com>
2021-02-03 23:46:53 +0300

    Final Project

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -10,6 +10,7 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        
 
         public ProductManager(IProductDal productDal)
         {
@@ -19,6 +20,16 @@ namespace Business.Concrete
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategory(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<= max);
         }
     }
 }
