5791e64bb7ea8f51253afe3aaaa57d4a10698fae
sametsolaklar <61630850+sametsolaklar@users.noreply.github.com>
2021-02-25 00:08:21 +0300

    2.25.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Product name is invalid.";
         public static string MaintenanceTime = "System is under maintenance";
         public static string ProductListed = "Product of Listed.";
+        public static string ProductCountOfCategoryError = "Max 10 product.";
+        public static string ProductNameAlreadyExists = "Product name is used.";
+        public static string CategoryLimitExceded = "Max 15 Category.";
     }
 }
